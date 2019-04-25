888bc28f43a92d31af30f1039267b8aa3941a43b
Francisco Schwertner <rockchico@gmail.com>
2019-04-25 12:16:14 -0300

    opa

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.123</p>
+    <p>Now go build something great.1234</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
